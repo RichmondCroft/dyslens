ce453959da370f98c958c05a4f3b908b51e74f5a
divvya007 <divvyatripaathi@gmail.com>
2022-04-07 01:12:46 +0100

    created new constants color and size

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,30 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import COLORS from "../../constants/Colors";
 
 const NavBarContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  background-color: #1f4070;
-  border: 1px solid black;
+  background-color: ${COLORS.DARK_BLUE};
   align-items: center;
   padding: 15px;
 `;
 
 const StyledDysLensImage = styled.div`
-  background-color: white;
   border-radius: 15px;
 `;
 
 const StyledDysLensTextBox = styled.div`
-  color: aliceblue;
+  color: ${COLORS.WHITE};
   font-weight: bold;
 `;
 
-const StyledSwitch = styled.div`
-  background-color: #33d464;
-  border-radius: 15px;
-`;
+const StyledSwitch = styled.div``;
 
 export default function NavigationBar() {
   return (
