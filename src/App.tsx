8390ab26a0008c76fb7413538f8ba200c24201fd
divvya007 <divvyatripaathi@gmail.com>
2022-04-08 17:34:07 +0100

    refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-import NavigationMenu from "./components/NavigationMenu";
-import NavigationBar from "./components/NavigationBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createBrowserHistory } from "history";
 
 import Home from "./pages/Home";
 import TextSettings from "./pages/TextSettings";
@@ -12,8 +7,6 @@ import OverlayTint from "./pages/OverlayTint";
 import LineFocus from "./pages/LineFocus";
 import Highlighter from "./pages/Highlighter";
 import HideImages from "./pages/HideImages";
-
-const history = createBrowserHistory();
 
 function App() {
   return (
