36383b6fd1012ee0f1dd8947fa5489c39b9e4626
divvya007 <divvyatripaathi@gmail.com>
2022-04-20 16:32:49 +0100

    chore:removed execute script and added insert css scripting api

--- src/chrome-utils/changeText.ts
+++ src/chrome-utils/changeText.ts
@@ -1,31 +1,13 @@
+let css = `@font-face {font-family: "OpenSans-Regular";
+ src: url("${chrome.runtime.getURL("OpenSans-Regular.ttf")}");}
+*{font-Family:"OpenSans-Regular" !important;
+letter-spacing:1px;}
+`;
+
 export default async function changeText() {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
+  chrome.scripting.insertCSS({
     target: { tabId: tab.id ? tab.id : 1111 },
-    func: getTextFont,
+    css: css,
   });
-}
-
-function getTextFont() {
-  var style = document.createElement("style");
-  const cssStyle = `
-    @font-face { 
-        font-family: "OpenSans-Regular"; 
-        src: url("${chrome.runtime.getURL("OpenSans-Regular.ttf")}");
-    }
-  `;
-  const styling = `
-  *{
-    font-Family:"OpenSans-Regular" !important;
-    letter-spacing:1px;
-  }
-  p{
-    font-weight: 400 !important;
-  }
-  
-  `;
-  style.appendChild(document.createTextNode(cssStyle));
-  style.appendChild(document.createTextNode(styling));
-
-  document.getElementsByTagName("head")[0].appendChild(style);
 }
