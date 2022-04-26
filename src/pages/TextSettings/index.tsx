3bf217bad1b618f362313f7a9cc3b64fc155aedb
divvya007 <divvyatripaathi@gmail.com>
2022-04-26 16:07:09 +0100

    added select options for font

--- src/pages/TextSettings/index.tsx
+++ src/pages/TextSettings/index.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import changeText from "../../chrome-utils/changeText";
-
+import COLORS from "../../constants/Colors";
+import SIZE from "../../constants/Size";
 const StyledTextSettingsContainer = styled.div`
   display: flex;
   flex-flow: column;
-  max-width: 150px;
-  height: 150px;
+  padding: ${SIZE.XX_SMALL}px ${SIZE.SMALL}px;
+  width: fit-content;
+  margin: auto;
+  background: ${COLORS.WARM_WHITE};
+  border: 1px solid ${COLORS.GRAY};
+  box-shadow: ${SIZE.ZERO}px ${SIZE.XX_SMALL}px ${SIZE.XX_SMALL}px
+    ${COLORS.SPECIAL};
 `;
 
 const StyledSliderContainer = styled.div`
@@ -25,7 +31,14 @@ function handleTextChange() {
 export default function TextSettings() {
   return (
     <StyledTextSettingsContainer data-testid="textSettingsContainer">
-      <button onClick={handleTextChange}>change text</button>
+      <p>The quick brown fox jumps over the lazy dog</p>
+      <div>
+        <select onChange={handleTextChange}>
+          <option value="0">Select Font:</option>
+          <option value="1">Open Sans</option>
+        </select>
+      </div>
+      {/* <button onClick={handleTextChange}>change text</button> */}
       <StyledSliderContainer>
         <label htmlFor="sizePx">Size:</label>
         <input type="range" id="sizePx" name="sizePx" min="0" max="100" />
