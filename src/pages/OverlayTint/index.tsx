5344e59a59376c7904068a8585513c6bca278bd2
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2022-05-04 23:56:25 +0100

    chore: added the enabled checkbox to the overlay panel

--- src/pages/OverlayTint/index.tsx
+++ src/pages/OverlayTint/index.tsx
@@ -2,12 +2,28 @@ import styled from "styled-components";
 
 import ColorPicker from "../../components/ColorPicker";
 import ComponentContainer from '../../components/ComponentContainer';
+import Toggle from "../../components/Toggle";
+import SIZE from "../../constants/size";
 
-const StyledOverLayTintContainer = styled.div``;
+const StyledOverLayTintContainer = styled.div`
+  padding: ${SIZE.X_SMALL}px;
+`;
+
+const Spacer = styled.div`
+  height: ${SIZE.SMALL}px;
+`
 
 export default function OverlayTint() {
+  function handleOnToggleStateChange() {
+    console.log('changed');
+  }
+
   return (
     <StyledOverLayTintContainer data-testid="overLayTintContainer">
+      <ComponentContainer label="Enabled">
+        <Toggle on={false} onStateChange={handleOnToggleStateChange} />
+      </ComponentContainer>
+      <Spacer />
       <ComponentContainer label="Color">
         <ColorPicker />
       </ComponentContainer>
