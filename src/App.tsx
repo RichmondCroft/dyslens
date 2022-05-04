062f388e5d220b73dd6de47da6c7da803b87ce61
divvya007 <divvyatripaathi@gmail.com>
2022-05-04 23:11:32 +0100

    removed app css file and added style to app function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { MemoryRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 
 import Home from "./pages/Home";
 import TextSettings from "./pages/TextSettings";
@@ -7,18 +8,25 @@ import LineFocus from "./pages/LineFocus";
 import HighLighter from "./pages/HighLighter";
 import HideImages from "./pages/HideImages";
 
+const StyledAppContainer = styled.div`
+  margin: 0px;
+  width: 370px;
+`;
+
 function App() {
   return (
-    <MemoryRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/text-settings" element={<TextSettings />}></Route>
-        <Route path="/overlay-tint" element={<OverlayTint />}></Route>
-        <Route path="/line-focus" element={<LineFocus />}></Route>
-        <Route path="/highlighter" element={<HighLighter />}></Route>
-        <Route path="/hide-images" element={<HideImages />}></Route>
-      </Routes>
-    </MemoryRouter>
+    <StyledAppContainer className="App">
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/text-settings" element={<TextSettings />}></Route>
+          <Route path="/overlay-tint" element={<OverlayTint />}></Route>
+          <Route path="/line-focus" element={<LineFocus />}></Route>
+          <Route path="/highlighter" element={<HighLighter />}></Route>
+          <Route path="/hide-images" element={<HideImages />}></Route>
+        </Routes>
+      </MemoryRouter>
+    </StyledAppContainer>
   );
 }
 
