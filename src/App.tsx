4b1760858c41fe5bca861b25e041959f7c1d6212
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2022-05-08 16:22:52 +0100

    chore: added async app state loading

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import OverlayTint from "./pages/OverlayTint";
 import LineFocus from "./pages/LineFocus";
 import HighLighter from "./pages/HighLighter";
 import HideImages from "./pages/HideImages";
+import StoreContext from "./storage/StoreContext";
+import { useEffect, useState } from "react";
+import COLORS from "./constants/colors";
 
 const StyledAppContainer = styled.div`
   margin: 0px;
@@ -15,20 +18,43 @@ const StyledAppContainer = styled.div`
   width: 370px;
 `;
 
+const initialState = {
+  enabled: false,
+  text: {
+  },
+  overlay: {
+    enabled: false,
+    color: COLORS.LIGHT_YELLOW
+  }
+}
+
 function App() {
+  const [storeLoaded, setStoreLoaded] = useState(false);
+  const [appState, setAppState] = useState(initialState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStoreLoaded(true)
+    }, 1000);
+  }, [])
+
   return (
-    <StyledAppContainer>
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/text-settings" element={<TextSettings />}></Route>
-          <Route path="/overlay-tint" element={<OverlayTint />}></Route>
-          <Route path="/line-focus" element={<LineFocus />}></Route>
-          <Route path="/highlighter" element={<HighLighter />}></Route>
-          <Route path="/hide-images" element={<HideImages />}></Route>
-        </Routes>
-      </MemoryRouter>
-    </StyledAppContainer>
+    storeLoaded === false ?
+      <div>Loading...</div> :
+      <StoreContext.Provider value={{ appState, setAppState }}>
+        <StyledAppContainer>
+          <MemoryRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/text-settings" element={<TextSettings />}></Route>
+              <Route path="/overlay-tint" element={<OverlayTint />}></Route>
+              <Route path="/line-focus" element={<LineFocus />}></Route>
+              <Route path="/highlighter" element={<HighLighter />}></Route>
+              <Route path="/hide-images" element={<HideImages />}></Route>
+            </Routes>
+          </MemoryRouter>
+        </StyledAppContainer>
+      </StoreContext.Provider>
   );
 }
 
