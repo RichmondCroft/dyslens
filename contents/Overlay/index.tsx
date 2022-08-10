be476eae783864b5670213a993d54432700174be
Digvijay Upadhyay <digvijay.u20@gmail.com>
2022-08-10 23:56:24 +0100

    refactor: refactored the content scripts and broke them into multiple modules

---
+++ contents/Overlay/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useMemo, useState } from "react";
+import type { PlasmoContentScript } from "plasmo";
+import cssText from "data-text:~/contents/Overlay/styles.css";
+
+export const config: PlasmoContentScript = {
+  matches: ["<all_urls>"]
+}
+
+export const getStyle = () => {
+  const style = document.createElement("style");
+  style.textContent = cssText;
+  return style;
+}
+
+const Overlay = () => {
+  const [overlaySettings, setOverlaySettings] = useState(null);
+
+  function handleOnStorageChange(changes, _areaName) {
+    const newState = changes.appState.newValue;
+    setOverlaySettings(newState.overlay);
+  }
+
+  useEffect(() => {
+    chrome.storage.sync.get('appState').then(({ appState }) => {
+      setOverlaySettings(appState.overlay);
+    });
+
+    chrome.storage.onChanged.addListener(handleOnStorageChange);
+  }, [setOverlaySettings, handleOnStorageChange]);
+
+  return (
+    overlaySettings &&
+    overlaySettings.enabled &&
+    <div
+      className="floating-overlay"
+      style={{
+        backgroundColor: overlaySettings.color,
+      }}
+    />
+  )
+}
+
+export default Overlay;
