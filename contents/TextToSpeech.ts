ca3cf3202ddba9fb3f1fc2f497d716574d220cbe
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2023-01-06 23:43:57 +0000

    feat: client side script to enable text to speech

---
+++ contents/TextToSpeech.ts
@@ -0,0 +1,45 @@
+function cancelSpeechIfSpeaking() {
+  if (speechSynthesis.speaking || speechSynthesis.pending) {
+    speechSynthesis.cancel();
+  }
+}
+
+async function handleOnMouseUp() {
+  // In some cases the text selection/de-selection will not happen immediately
+  // or synchronously to handle this situation I have added timer
+  setTimeout(async () => {
+    cancelSpeechIfSpeaking();
+
+    var selectedText = window.getSelection().toString();
+    if (!selectedText.trim()) {
+      return;
+    }
+
+    const { appState } = await chrome.storage.sync.get('appState');
+
+    let utterance = new SpeechSynthesisUtterance(selectedText);
+    utterance.pitch = appState?.textToSpeech?.pitch || 1;
+    utterance.rate = appState?.textToSpeech?.rate || 1;
+    utterance.volume = appState?.textToSpeech?.volume || 1;
+    utterance.voice = appState?.textToSpeech?.voice || null;
+
+    speechSynthesis.speak(utterance);
+  }, 10);
+}
+
+async function applyTextToSpeechSettings() {
+  const { appState } = await chrome.storage.sync.get('appState');
+  if (appState && appState.textToSpeech && appState.textToSpeech.enabled) {
+    document.addEventListener('mouseup', handleOnMouseUp);
+  }
+  else {
+    document.removeEventListener('mouseup', handleOnMouseUp);
+  }
+}
+
+(function () {
+  chrome.storage.onChanged.addListener(applyTextToSpeechSettings);
+  applyTextToSpeechSettings();
+})()
+
+export { };
